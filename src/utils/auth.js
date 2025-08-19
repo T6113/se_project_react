2607cefa87b54896854c33dae026d28551bed418
T6113 <thomishamyers@gmail.com>
2025-08-18 20:26:23 -0500

    Remove code duplication: import checkResponse from api.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,6 @@
-const baseUrl = "http://localhost:3001";
+import { checkResponse } from "./api";
 
-export function checkResponse(res) {
-  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-}
+const baseUrl = "http://localhost:3001";
 
 // User registration
 export function signup({ name, avatar, email, password }) {
